5a67435c00ecee874177e8202cf2476251e9ef33
Malte-Maurice Dreyer <dreyer.maltem@gmail.com>
2014-07-14 22:08:38 +0000

    Increased SelectorList test complexity

--- test/list.js
+++ test/list.js
@@ -6,11 +6,11 @@ exports.selectorList = {
     "test": function(test) {
         var selector;
 
-        selector = new SelectorList(".example :nth-child(odd), .test"); // create selector
+        selector = new SelectorList(".example :matches(:nth-child(1), :nth-child(even)), .test"); // create selector
         test.deepEqual(selector.find({
             "example": ["this is the first child", "some other child", "this is the last child"],
             "test": "whatever"
-        }), ["this is the first child", "this is the last child", "whatever"]);
+        }), ["this is the first child", "some other child", "whatever"]);
 
         test.done();
     }
