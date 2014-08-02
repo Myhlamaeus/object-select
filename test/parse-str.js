89828062e39e495370087888fbd9a0b84edc1e00
Malte-Maurice Dreyer <dreyer.maltem@gmail.com>
2014-08-02 23:17:50 +0000

    Fixes #1

--- test/parse-str.js
+++ test/parse-str.js
@@ -5,6 +5,8 @@ var parseStr = require("../lib/tools").parseStr;
 exports.simpleSelector = {
     "test": function(test) {
         test.deepEqual(parseStr("abc, def, \"ghi\", .test, .\"te st\", (abc, def)"), ["abc", "def", "ghi", ".test", ".\"te st\"", "(abc, def)"]);
+        test.deepEqual(parseStr("\"ghi\" .test .\"te st\"", [" "]), ["ghi", ".test", ".\"te st\""]);
+        test.deepEqual(parseStr("abc.\"de f\":ghi", [".", ":"], true), ["abc", ".\"de f\"", ":ghi"]);
 
         test.done();
     }
